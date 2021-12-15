aca39d20a3056f57ba961499d8953621a87cd448
douglas.wender <douglas.wender@rarolabs.com.br>
2021-12-14 22:47:09 -0400

    remove unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:vagas_esports/src/app/app_module.dart';
 import 'package:vagas_esports/src/app/app_widget.dart';
-import 'package:vagas_esports/src/config/database/database_supabase_client.dart';
 
 void main() {
   return runApp(ModularApp(module: AppModule(), child: AppWidget()));
